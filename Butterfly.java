aa6a9308e0e8e3f1c040de9eeb8efaf4c845d38b
achankrishna <achankrishna@gmail.com>
2021-07-15 02:07:24 +0800

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,7 @@
+package com.singtel.behavioural;
+
+import com.singtel.bird.Bird;
+
+public class Butterfly extends Bird {
+
+}
---
+++ Catterpiller.java
@@ -0,0 +1,9 @@
+package com.singtel.behavioural;
+
+import com.singtel.animal.Animal;
+
+public class Catterpiller extends Animal {
+    public void walk(){
+        System.out.println("I'm crawling");
+    }
+}
